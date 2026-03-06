bbf6c53ff129b06ac6eeca71c9d0adbef01810ee
Luce Zavaglia <LUCE.ZAVAGLIA@local.ittcesena.it>
2026-03-06 09:49:53 +0100

    prima versione finita. Stampa lettere casuali ad ogni click del bottone

--- ProgAsincrona/MainWindow.xaml.cs
+++ ProgAsincrona/MainWindow.xaml.cs
@@ -20,5 +20,26 @@ namespace ProgAsincrona
         {
             InitializeComponent();
         }
+
+        Random random = new Random();
+        List<char> lettere = new List<char>();
+        private async void btnGeneraLettere_Click(object sender, RoutedEventArgs e)
+        {
+            while (true)
+            {
+                int numero = random.Next(0, 26);
+                char lettera = (char)('A' + numero);
+                lblLettereCasuali.Content = lettera.ToString();
+                lettere.Add(lettera);
+                await Task.Delay(200);
+            }
+        }
+
+        private void btnStampaLettere_Click(object sender, RoutedEventArgs e)
+        {
+            char letteraDaStampare = lettere[lettere.Count - 1];
+            //lblStampaLettere.Content = letteraDaStampare.ToString(); se faccio così ogni volta che si clicca la lettera viene sovrascritta
+            lblStampaLettere.Content += letteraDaStampare.ToString();
+        }
     }
 }
